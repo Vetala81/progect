75ebedf85bb774c326888c2ab005fe7bf985c676
vetal <vetala1981@gmail.com>
2025-11-12 21:25:15 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,87 @@
+"use strict" ;
+
+//alert ('hello');
+
+//const result = confirm('Are you hear ?');
+//console.log(result);
+
+//const answer = +prompt('Вам есть 18?', '18');
+//console.log(typeof(answer));
+/*
+const answers = [];
+
+answers[0] = prompt('Как ваше имя', '');
+answers[1] = prompt('Как ваша фамилия ', '');
+answers[2] = prompt('Какой ваш номер телефона', '');
+
+console.log(typeof(answers));
+
+
+
+
+/*
+const arr = ['a', 'b', 'c'];
+const objNew = {
+    0: 'a',
+    1: 'b',
+    2: 'c'
+};
+
+const b = 'b'
+//objNew.b = '1231';
+objNew[b] = '1231';
+
+console.log(objNew ['b']);
+
+//const obj = {a: 1, b: 2};
+
+const obj = {
+    Anna: 500,
+    'Alica': 800
+};
+console.log(obj);
+
+
+/*
+const obj = {
+    name: 'John',
+    age: 24,
+    isMarryed: false
+};
+
+
+
+console.log(obj.age);
+console.log(obj ['isMarryed']);
+
+let arr = ['plan.png', 'apple.bmp', 6, 'orange.jpg', {}, []];
+
+console.log(arr[1])
+
+
+
+
+
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);
+
+const user = 'Vet' ;
+
+alert(`Hello ${user}`);
+*/
+
+let incr = 10,
+    decr = 10;
+
+//++incr;
+//--decr;
+
+console.log(++incr);
+console.log(--decr);
+
+
+const isCheked = false,
+      isClose = false;
+      
+console.log(isCheked || !isClose);      
